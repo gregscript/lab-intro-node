38c7661772bcb5c3ac6e2329def12a4a64c5fea4
Greg <gajdukow@protonmail.com>
2024-04-29 16:41:14 +0200

    solved lab

--- index.js
+++ index.js
@@ -1,17 +1,67 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    // console.log(`......`);
+    // console.log(`add invoked with ${item}`);
+    // console.log(`......`);
+    this.items.push(item);
+    this.items.sort((a,b) => {return a-b});
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos >= 0 && pos < this.length) return this.items[pos];
+    throw new Error("OutOfBounds");
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0) throw new Error("EmptySortedList");
 
-  min() {}
+    let max = this.items.reduce((accumulator, current) => {
+      return accumulator > current ? accumulator : current
+    })
+    return max;
+  }
 
-  sum() {}
+  min() {
+    if(this.length === 0) throw new Error("EmptySortedList"); 
 
-  avg() {}
+    let min = this.items.reduce((accumulator, current) => {
+      return accumulator < current ? accumulator : current
+    })
+    console.log(min)
+    return min;
+
+  }
+
+  sum() {
+    if(this.length === 0) return 0;
+
+    let sum = this.items.reduce((acc, cur) => 
+    { return acc + cur})
+    return sum;
+  }
+
+  avg() {
+    if(this.length === 0) throw new Error("EmptySortedList"); 
+
+    let sum = this.items.reduce((acc, cur) => 
+    { return acc + cur})
+    return sum/this.length;
+
+  }
 }
+
+let testList = new SortedList();
+testList.add(2);
+testList.add(5);
+testList.add(7);
+console.log("Position 2:", testList.get(2))
+console.log(testList.items)
+console.log("max:", testList.max())
 
 module.exports = SortedList;
